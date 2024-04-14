cdf31b0cb25bddabd50b3b4dd578bb987041049d
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-14 21:02:05 +0200

    add technology seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -16,6 +16,18 @@ class TechnologySeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+        $techs = ['HTML5', 'CSS3', 'Javascript ES5', 'VueJS 3', 'PHP', 'Json', 'Laravel', 'Bootstrap', 'Blade', 'Git'];
+
+
+        foreach($techs as $tech) {
+            $technology = new Technology();
+
+            $technology->label = $tech;
+            $technology->color = $faker->hexColor();
+
+            $technology->save();
+        }
+
+
     }
 }
