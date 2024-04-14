2e2f8f168659af228ca90e6ee3491f401297d62b
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-14 21:14:59 +0200

    add parameters to ProjectTechnologySeeder

--- database/seeders/ProjectTechnologySeeder.php
+++ database/seeders/ProjectTechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectTechnologySeeder extends Seeder
 {
@@ -12,8 +15,16 @@ class ProjectTechnologySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $projects = Project::all();                       // object Post
+        $technologies = Technology::all()->pluck('id')->toArray(); // array  [1, 2, ... n]
+
+        foreach ($projects as $project) {
+            $project
+                ->technologies()
+                ->attach($faker->randomElements($technologies, random_int(0, 3)));
+        }
+
     }
 }
