7054a69f9f92559631ae49d06e359147b299a237
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-14 20:48:29 +0200

    add technology seeder

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use Faker\Generator as Faker;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //
+    }
+}
