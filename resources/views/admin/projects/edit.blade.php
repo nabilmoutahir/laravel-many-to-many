b2cb564f7de87a26b7580dfe5cf9de5390c3143b
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-17 20:44:15 +0200

    add edit view

---
+++ resources/views/admin/projects/edit.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+
+
+  <form action="{{ route('admin.projects.update', $project) }}" method="POST" class="row d-flex">
+    @method('PATCH')
+
+    @csrf
+
+    <div class="col-6">
+
+      {{-- <div class="col-12">
+        <label for="type_id" class="form-label">Type</label>
+        <select name="type_id" id="type_id" class="form-select @error('type_id') is-invalid @enderror">
+        <option value="">Non categorizzato</option>
+        @foreach ($types as $type)
+          <option value="{{ $type->id }}" @if (old('type_id') == $type->id) selected @endif>
+            {{ $type->label }}
+          </option>
+        @endforeach
+        </select>
+        @error('type_id')
+          <div class="invalid-feedback">
+            MEX
+          </div>
+        @enderror
+      </div> --}}
+
+      {{-- TITLE --}}
+      <div class="col-12">
+          <label class="form-label" for="title">Title</label>
+          <input class="form-control" type="text" name="title" id="title" value="{{ $project['title'] }}" >
+      </div>
+
+      {{-- CONTENT --}}
+      <div class="col-12">
+          <label for="content">Content</label>
+          <textarea class="form-control" rows="12" type="text" name="content" id="content">
+            {{ $project['content'] }}
+          </textarea>
+      </div>
+
+      {{-- SAVE --}}
+      <div class="col-3 pt-3">
+          <button class="btn btn-primary">Save!</button>
+      </div>
+    </div>
+
+    {{-- <div class="col-6">
+      <label class="form-label">Technologies</label>
+      @foreach ($technologies as $tech)
+      <div class="col-12">
+        
+        <input class="form-check-input" id="technologies-{{ $tech->id }}" value="{{ $tech->id }}" name="technologies[]" type="checkbox">
+        <label class="form-check-label" for="technologies-{{ $tech->id }}">{{ $tech->label }}</label>
+        
+      </div>
+      @endforeach
+    </div> --}}
+  </form>
+
+
+</div>
+
+@endsection
